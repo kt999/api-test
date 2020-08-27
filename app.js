f20b821bc4bc37f5805ace3ae59dd39a3c422390
kt999 <kiteak9275@gmail.com>
2020-08-27 22:47:53 +0900

    test1

--- app.js
+++ app.js
@@ -12,41 +12,69 @@ if (!PORT) {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-///////////// front routing
+app.use(bodyParser.json());
 
-app.get("/movie", (req, res) => {
+///////////// server routing
+
+const userList = [
+  {
+    id: "test1",
+    password: "abc1234",
+  },
+  {
+    id: "test2",
+    password: "abc1234",
+  },
+  {
+    id: "test3",
+    password: "abc1234",
+  },
+  {
+    id: "test4",
+    password: "abc1234",
+  },
+  {
+    id: "test5",
+    password: "abc1234",
+  },
+];
+
+app.get("/test", (req, res) => {
   res.json({
-    result: [
-      {
-        idx: 1,
-        imgUrl:
-          "https://post-phinf.pstatic.net/MjAxODExMzBfMjUz/MDAxNTQzNTc3MjUyOTM1.-Ye3_yI0tXYsAuXNtkxbVCi_yGOVo2rvGAjWByFh9EEg.8QPa5B8_9S2SWmUkBFL2qesAYP7sOcsqeNkBxCNmgeMg.JPEG/%ED%82%B9%EB%8D%A4_%ED%8B%B0%EC%A0%80%ED%8F%AC%EC%8A%A4%ED%84%B0.jpg?type=w1200",
-      },
-      {
-        idx: 2,
-        imgUrl:
-          "https://lh3.googleusercontent.com/proxy/lzdr2eXnP2IQ-mJioB7ylkLsT6CO0WXmry4r9k2fKbnW3e-r7T99YW_2ddnr_79a8URuALO9zAZmlR7T_Rt9h5aSDTMUlhfEJqzD1zIcDHOqYzOrYz_TzpHphj4V9SifZNKFLFPulDN847tf1i9cOztXxtHolVqNgag1WvJoxrMGHVv_8UQzMFv-8uCY63-BiS552NDU1Tnuy_8ER5-_kfVduLUUv1EKiT-QjUvEugmZ4Z3vi3VCr5oKctkeafgJOax5fRcPgxKrH1sg-XuafAtE2So712FuK-NQeo-zejK-S0p8kk_bRA",
-      },
-      {
-        idx: 3,
-        imgUrl:
-          "https://ojsfile.ohmynews.com/PHT_IMG_FILE/2018/0319/IE002302766_PHT.jpg",
-      },
-      {
-        idx: 4,
-        imgUrl:
-          "http://img.mediapen.com/news/201812/news_403283_1545358438_m.jpg",
-      },
-      {
-        idx: 5,
-        imgUrl:
-          "https://pgnqdrjultom1827145.cdn.ntruss.com/img/fd/55/fd55751e6e7d09d0317ba72e3fff7cd76236ef355d48e597e124e17cd4dfeb59_v1.jpg",
-      },
-    ],
+    result: "hihi",
   });
 });
 
-///////////// server routing
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  var id = req.body.id;
+  var password = req.body.password;
+
+  console.log(id);
+  console.log(password);
+
+  var status = 0;
+
+  for (var i = 0; i < userList.length; i++) {
+    if (userList[i].id == id && userList[i].password == password) {
+      status = 1;
+    }
+  }
+
+  if (status == 1) {
+    //로그인 성공
+
+    res.json({
+      result: "성공",
+    });
+  } else {
+    //로그인 실패
+
+    res.json({
+      result: "실패",
+    });
+  }
+});
 
 ///////////////////
 http.listen(PORT, () => {
